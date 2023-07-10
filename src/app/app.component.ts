5ef40f817cca1277a7ba0d3b6981266e89192dc3
asiurniene <ada.siurniene@innoforcegroup.com>
2023-07-10 09:50:55 +0300

    destroying modal created by our service to prevent memory leak

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ModalService } from './services/modal.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'clips';
+export class AppComponent implements OnInit {
+    showModal = true
+//   title = 'clips';
+
+    constructor(public modal: ModalService) {
+        
+    }
+
+    ngOnInit() {
+        setInterval(() => {
+            this.showModal = !this.showModal
+            console.log('modals in app component:', this.modal.modals);
+        }, 1000)
+    }
 }
