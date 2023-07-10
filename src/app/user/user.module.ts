3761de02f58ea3d514194436466540f2bdd46a49
asiurniene <ada.siurniene@innoforcegroup.com>
2023-07-11 00:31:39 +0300

    form input component with validation and err messages

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthModalComponent } from './auth-modal/auth-modal.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { SharedModule } from '../shared/shared.module';
 import { LoginComponent } from './login/login.component';
@@ -15,6 +16,7 @@ import { RegisterComponent } from './register/register.component';
   imports: [
     CommonModule,
     SharedModule,
+    ReactiveFormsModule,
   ],
   exports: [
     AuthModalComponent
